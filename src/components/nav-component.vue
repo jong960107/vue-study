b557dba5976773d19c4b1aab9c31c877f317fcdf
jong960107 <jong960107@naver.com>
2022-03-10 21:32:03 +0900

    [#3]뷰 라우터 링크

--- src/components/nav-component.vue
+++ src/components/nav-component.vue
@@ -1,8 +1,18 @@
 <template>
   <nav class="nav">
     <ul>
-      <li><h2>Members</h2></li>
-      <li><h2>Search</h2></li>
+      <li>
+        <h2>
+          <router-link :to="{ name: 'Members' }" active-class="active"
+            >Members</router-link
+          >
+        </h2>
+      </li>
+      <li>
+        <h2>
+          <router-link :to="{ name: 'Search' }" active-class="active">Search</router-link>
+        </h2>
+      </li>
     </ul>
   </nav>
 </template>
